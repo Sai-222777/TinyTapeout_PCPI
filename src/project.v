c12e8d5462bcb448452ee73c7e16824b4269316b
Sai-222777 <122385607+Sai-222777@users.noreply.github.com>
2025-09-07 15:58:43 +0530

    Update project.v
    Uncommented PCPI Unit and FSM

--- src/project.v
+++ src/project.v
@@ -24,10 +24,10 @@ module tt_um_Sai_222777 (
     assign unused = uio_in;
     
     wire [3:0] instruction_segment;
-    // wire sending_current,received_current;
+    wire sending_current,received_current;
     assign instruction_segment = ui_in[4:1];
-    // assign sending_current = ui_in[0];
-    assign uo_out = 8'd0;// assign uo_out = {7'd0,received_current};
+    assign sending_current = ui_in[0];
+    assign uo_out = {7'd0,received_current};
     
     reg pcpi_valid;
     reg [31:0] instruction_latched;
@@ -36,33 +36,33 @@ module tt_um_Sai_222777 (
     wire pcpi_wait, pcpi_wr;
     wire [31:0] pcpi_rd;
     
-    // fused_matrix_mult_pcpi pcpi_unit(
-    //     .clk(clk),
-    //     .resetn(rst_n),
-    //     .pcpi_valid(pcpi_valid),
-    //     .pcpi_insn(instruction_latched),
-    //     .pcpi_ready(pcpi_ready),
-    //     .pcpi_wr(pcpi_wr),
-    //     .pcpi_wait(pcpi_wait),
-    //     .pcpi_rd(pcpi_rd)
-    // );
+    fused_matrix_mult_pcpi pcpi_unit(
+        .clk(clk),
+        .resetn(rst_n),
+        .pcpi_valid(pcpi_valid),
+        .pcpi_insn(instruction_latched),
+        .pcpi_ready(pcpi_ready),
+        .pcpi_wr(pcpi_wr),
+        .pcpi_wait(pcpi_wait),
+        .pcpi_rd(pcpi_rd)
+    );
 
     assign uio_out = {7'd0,pcpi_wait};
     
     reg [2:0] count;
     genvar e;
-    // generate
-    //     for(e=0;e<8;e=e+1)
-    //     begin
-    //         always @(posedge clk)
-    //         begin
-    //             if(received_current && e==count)
-    //             begin
-    //                 instruction_latched[4*(e+1)-1:4*e] <= instruction_segment;
-    //             end
-    //         end
-    //     end
-    // endgenerate
+    generate
+        for(e=0;e<8;e=e+1)
+        begin
+            always @(posedge clk)
+            begin
+                if(received_current && e==count)
+                begin
+                    instruction_latched[4*(e+1)-1:4*e] <= instruction_segment;
+                end
+            end
+        end
+    endgenerate
 
     // always @(posedge clk) 
     // begin
@@ -74,54 +74,54 @@ module tt_um_Sai_222777 (
 
     
     reg [1:0] state;
-    // assign received_current = state == 2'b01;
+    assign received_current = state == 2'b01;
     
-    // always @(posedge clk)
-    // begin
-    //     if(!rst_n)
-    //     begin
-    //         count <= 0;
-    //         state <= 2'b00;
-    //         pcpi_valid <= 0;
-    //     end
-    //     else
-    //     begin
-    //         case(state)
-    //             2'b00 :
-    //             begin
-    //                 if(sending_current)
-    //                 begin
-    //                     state <= 1;
-    //                 end
-    //             end
-    //             2'b01 :
-    //             begin
-    //                 if(count < 7)
-    //                 begin
-    //                     count <= count + 1;
-    //                     state <= 0;
-    //                 end
-    //                 else
-    //                 begin
-    //                     count <= 0;
-    //                     state <= 2; //state <= 2; should actually be 2, but changed it for testbench simulation
-    //                     pcpi_valid <= 1;
-    //                 end
-    //             end
-    //             2'b10:
-    //             begin
-    //                 pcpi_valid <= 0;
-    //                 state <= 3;
-    //             end
-    //             2'b11:
-    //             begin
-    //                 if(pcpi_ready)
-    //                 begin
-    //                     state <= 0;
-    //                 end
-    //             end
-    //         endcase
-    //     end
-    // end
+    always @(posedge clk)
+    begin
+        if(!rst_n)
+        begin
+            count <= 0;
+            state <= 2'b00;
+            pcpi_valid <= 0;
+        end
+        else
+        begin
+            case(state)
+                2'b00 :
+                begin
+                    if(sending_current)
+                    begin
+                        state <= 1;
+                    end
+                end
+                2'b01 :
+                begin
+                    if(count < 7)
+                    begin
+                        count <= count + 1;
+                        state <= 0;
+                    end
+                    else
+                    begin
+                        count <= 0;
+                        state <= 2; //state <= 2; should actually be 2, but changed it for testbench simulation
+                        pcpi_valid <= 1;
+                    end
+                end
+                2'b10:
+                begin
+                    pcpi_valid <= 0;
+                    state <= 3;
+                end
+                2'b11:
+                begin
+                    if(pcpi_ready)
+                    begin
+                        state <= 0;
+                    end
+                end
+            endcase
+        end
+    end
     
 endmodule
