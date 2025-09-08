2e989bc151e8abf623f2dc901ad0d144b712324a
Sai-222777 <122385607+Sai-222777@users.noreply.github.com>
2025-09-08 16:05:39 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -60,7 +60,7 @@ endmodule
 
 // `default_nettype none
 
-// module tt_um_Sai_222777 (
+// module tt_um_Saii_222777 (
 //     input  wire [7:0] ui_in,    // Dedicated inputs
 //     output wire [7:0] uo_out,   // Dedicated outputs
 //     input  wire [7:0] uio_in,   // IOs: Input path
