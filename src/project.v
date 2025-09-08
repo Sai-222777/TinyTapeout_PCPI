ece7a79e5eb451beb7a04e727fac39b27625ceca
Sai-222777 <122385607+Sai-222777@users.noreply.github.com>
2025-09-08 22:54:52 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -69,12 +69,11 @@ module tt_um_Sai_222777 (
         if(received_current) 
         begin
             case(count)
-                begin
                     3'b000:
                     begin
                         instruction_latched[4:0] <= instruction_segment;
                     end
-                end
+            endcase
         end
     end
     
